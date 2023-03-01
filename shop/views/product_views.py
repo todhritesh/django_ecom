15049e138f6594e8a265a6e41bf36e42e63f4c40
todhritesh <todhritesh@gmail.com>
2023-03-01 10:57:17 +0530

    added search

--- shop/views/product_views.py
+++ shop/views/product_views.py
@@ -3,37 +3,37 @@ from ..models import *
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required , user_passes_test
 from django.urls import reverse_lazy
-from django.db.models import Case , When , BooleanField
+from django.db.models import Case , When , BooleanField , Q
 
 
 def products(req):
+    context = {}
     user = req.user
     categories = Category.objects.all()
     products = ''
     cat = req.GET.get('category',None)
     if(cat!=None):
-        if(user.is_authenticated):
-            products = Product.objects.filter(category__slug=cat).annotate(isWishlisted=Case(
-                When(wishlist__user=user , then = True) ,
-                default=False,
-                output_field=BooleanField()
-            ))
-        else:
-            products = Product.objects.filter(category__slug=cat)
+        context['cat'] = cat
+        products = Product.objects.filter(category__slug=cat)
+    elif req.GET.get('search',None) != None:
+        search = req.GET.get('search',None)
+        context['search'] = search
+        products = Product.objects.filter(Q(product_title__icontains=search)|Q(category__title__icontains=search))
     else:
-        if(user.is_authenticated):
-            products = Product.objects.all().annotate(isWishlisted=Case(
-                When(wishlist__user=user , then = True) ,
-                default=False,
-                output_field=BooleanField()
-            ))
-        else:
-            products = Product.objects.all()
+        products = Product.objects.all()
+
+    if user.is_authenticated:
+        products = products.annotate(isWishlisted=Case(
+                    When(wishlist__user=user , then = True) ,
+                    default=False,
+                    output_field=BooleanField()
+                ))
 
     paginator = Paginator(products, 50)
     pg_no = req.GET.get('page',1)
     data = paginator.get_page(pg_no)
-    context = {'data':data,'categories':categories,'cat':cat}
+    context['data'] = data
+    context['categories'] = categories
 
     return render(req, 'pages/products.html',context=context)
 
